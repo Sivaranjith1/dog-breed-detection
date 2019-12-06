b4022a16733ce6fbe42326a21a77e081bb9a87ea
Sivranjith Sivarasa <ranjith1210000@gmail.com>
2019-12-06 23:30:51 +0100

    image reading

--- dataPreprossesing.py
+++ dataPreprossesing.py
@@ -2,21 +2,26 @@ import csv
 import numpy as np
 import cv2
 import os
+from matplotlib import pyplot as plt
 
 LABEL_CSV = 'dataset/labels.csv'
-TRAINING_DATA= 'dataset/train'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+TRAINING_DATA= os.path.join(dir_path, "dataset\\train\\")
+
 IMG_SIZE = 50
 
 X = []
 Y = []
 
-def getImageData(training_folder, id):
-    path = os.path.join(training_folder, id)
+def getImageData(training_folder, img_id):
+    pathToImage = os.path.join(training_folder, img_id)
     try:
-        
+        image = cv2.imread(pathToImage, cv2.IMREAD_GRAYSCALE)
+        # image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))        
+        print(image)
     except Exception as e:
+        print(e)
         pass
-
     return image
 
 with open(LABEL_CSV) as csv_file:
@@ -30,3 +35,6 @@ with open(LABEL_CSV) as csv_file:
             Y.append(row[1])
             X.append(getImageData(TRAINING_DATA, row[0]))
     print(f'Processed {line_count} lines.')
+
+print(X[0])
+# plt.imshow(X[0])
